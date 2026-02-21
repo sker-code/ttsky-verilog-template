aea7a00bfd5af91cd6959ea9af3c8de58ab74e20
sylviaker <sylviaker720@gmail.com>
2026-02-21 14:00:35 -0500

    test1

--- src/RangeFinder.sv
+++ src/RangeFinder.sv
@@ -1,44 +1,6 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
+// Task 1
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
-
-  // All output pins must be assigned. If not used, assign to 0.
-  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  // assign uio_out = 0;
-  // assign uio_oe  = 0;
-
-  // uio[0] = 0, uio[1] = 0, uio[2] = 1
-  always_comb begin
-    uio_oe = 7'b0010_0000;
-    uio_out[3:7] = 0;
-    uio_out[0:1] = 0;
-  end
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{uio_in[2:7], ena, '0};
-
-  //RangeFinder
-  RangeFinder #(8) rf(.data_in(ui_in), .clock(clk), .reset(~rst_n), .go(uio_in[0]), .finish(uio_in[1]),
-                      .range(uo_out), .error(uio_out[2]));
-
-endmodule
-
-
-// RangeFinder
 module RangeFinder
   #(parameter WIDTH=16)
   (input  logic [WIDTH-1:0] data_in,
